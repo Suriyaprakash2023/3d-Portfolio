918c2e4a0d0612ff2820759e48e6c7d0c46c8e36
Suriyaprakash2023 <150240754+Suriyaprakash2023@users.noreply.github.com>
2024-07-21 07:51:48 +0530

    hero page 70% completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,51 +1,114 @@
-import { motion } from "framer-motion";
+// import React from 'react'
+import HeroBgAnimation from './HeroBgAnimation'
+// import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
+import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle, ResumeButton } from './HeroStyle'
+// import HeroImg from '../../images/HeroImage.jpg'
+import HeroImg from './images/HeroImage1.webp';
+import Typewriter from 'typewriter-effect';
+// import { Bio } from '../../data/constants';
+import { Bio } from './data/constants';
+import { Link } from 'react-router-dom';
+import { RiContactsFill } from "react-icons/ri";
+import { MdDownload } from "react-icons/md";
+import './Hero.css';
 
-import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
+import { FaFacebook, FaTwitterSquare } from "react-icons/fa";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { SiLeetcode } from "react-icons/si";
 
-const Hero = () => {
-  return (
-    <section className={`relative w-full h-screen mx-auto`}>
-      <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
-      >
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+// import { SiLeetcode } from "react-icons/si";
+// ...
+
+
+const HeroSection = () => {
+    return (
+        <div id="about">
+            <HeroContainer>
+                <HeroBg>
+                    <HeroBgAnimation />
+                </HeroBg>
+                <HeroInnerContainer >
+                    <HeroLeftContainer id="Left">
+                        {/* <Title>Hi, I am <br /> <Span className='pink-font'> {Bio.name} </Span></Title> */}
+                        <TextLoop>
+                            I am a
+                            <Span className='green-font'>
+                                <Typewriter
+                                    options={{
+                                        strings: Bio.roles,
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
+                            </Span>
+                        </TextLoop>
+                        <SubTitle>{Bio.description}</SubTitle>
+                        {/* <ResumeButton className='gradient-button' href={Bio.resume} target='display'>Check Resume</ResumeButton> */}
+                        <div className="my-3 ">
+                            <Link
+                            href={Bio.github}
+                            target='_blank'
+                            className=" icon-link"
+                            >
+                            <BsGithub size={30} />
+                            </Link>
+                            <Link
+                            href={Bio.linkedIn}
+                            target='_blank'
+                            className="icon-link"
+                            >
+                            <BsLinkedin size={30} />
+                            </Link>
+                            <Link
+                            href={Bio.facebook}
+                            target='_blank'
+                            className="icon-link"
+                            >
+                            <FaFacebook size={30} />
+                            </Link>
+                            <Link
+                            href={Bio.leetcode}
+                            target='_blank'
+                            className="icon-link"
+                            >
+                            <SiLeetcode size={30} />
+                            </Link>
+                            <Link
+                            href={Bio.twitter}
+                            target='_blank'
+                            className="icon-link"
+                            >
+                            <FaTwitterSquare size={30} />
+                            </Link>
+                        </div>
+                          
+                        <div className="flex items-center gap-3">
+                            <Link href="#contact" className="bg-gradient-to-r to-pink-500 from-violet-600 p-[1px] rounded-full transition-all duration-300 hover:from-pink-500 hover:to-violet-600">
+                            <div className='outline-button'>  
+                                  <button className='btn text-white ' >
+                                    <span>Contact me</span>
+                                    <RiContactsFill size={16} />
+                                </button>
+                            </div>
+                            </Link>
+
+                            <Link className='gradient-button ms-3' role="button" 
+                            >
+                            <span >Get Resume</span>
+                            <MdDownload size={16} />
+                            </Link>
+                        </div>
+                    </HeroLeftContainer>
+
+                    <HeroRightContainer id="Right">
+
+                        <Img src={HeroImg} alt="hero-image" />
+                    </HeroRightContainer>
+                </HeroInnerContainer>
+
+            </HeroContainer>
         </div>
+    )
+}
 
-        <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
-          </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
-          </p>
-        </div>
-      </div>
-
-      <ComputersCanvas />
-
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
-      </div>
-    </section>
-  );
-};
-
-export default Hero;
+export default HeroSection
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,102 +1,61 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-
-import { styles } from "../styles";
-import { navLinks } from "../constants";
-import { logo, menu, close } from "../assets";
-
+import React from 'react'
+import { Nav, NavLink, NavbarContainer, Span, NavLogo, NavItems, GitHubButton, ButtonContainer, MobileIcon, MobileMenu, MobileNavLogo, MobileLink } from './NavbarStyledComponent'
+import { DiCssdeck } from 'react-icons/di';
+import { FaBars } from 'react-icons/fa';
+// import { Bio } from '../data/constants';
+// import { Close, CloseRounded } from '@mui/icons-material';
+import { useTheme } from 'styled-components';
+import './Navbar.css';
 const Navbar = () => {
-  const [active, setActive] = useState("");
-  const [toggle, setToggle] = useState(false);
-  const [scrolled, setScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.scrollY;
-      if (scrollTop > 100) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
+  const [isOpen, setIsOpen] = React.useState(false);
+  // const theme = useTheme()
   return (
-    <nav
-      className={`${
-        styles.paddingX
-      } w-full flex items-center py-5 fixed top-0 z-20 ${
-        scrolled ? "bg-primary" : "bg-transparent"
-      }`}
-    >
-      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-        <Link
-          to='/'
-          className='flex items-center gap-2'
-          onClick={() => {
-            setActive("");
-            window.scrollTo(0, 0);
-          }}
-        >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Adrian &nbsp;
-            <span className='sm:block hidden'> | JavaScript Mastery</span>
-          </p>
-        </Link>
+    <Nav>
+      <NavbarContainer>
+        <NavLogo to='/'>
+          <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
+            <DiCssdeck size="3rem" /> <Span className='nav-name'>Suriya</Span>
+          </a>
+        </NavLogo>
+        <MobileIcon>
+          <FaBars onClick={() => {
+            setIsOpen(!isOpen)
+          }} />
+        </MobileIcon>
+        <NavItems>
+          <NavLink href="#about" className="nav-link">About</NavLink>
+          <NavLink href='#skills'className="nav-link">Skills</NavLink>
+          <NavLink href='#experience' className="nav-link">Experience</NavLink>
+          <NavLink href='#projects' className="nav-link">Projects</NavLink>
+          <NavLink href='#education' className="nav-link">Education</NavLink>
+        </NavItems>
+        <ButtonContainer>
+          <GitHubButton className='github-button'  target="_blank">Github Profile</GitHubButton>
+        </ButtonContainer>
+        {
+          isOpen &&
+          <MobileMenu isOpen={isOpen}>
+            <MobileLink href="#about" onClick={() => {
+              setIsOpen(!isOpen)
+            }}>About</MobileLink>
+            <MobileLink href='#skills' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Skills</MobileLink>
+            <MobileLink href='#experience' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Experience</MobileLink>
+            <MobileLink href='#projects' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Projects</MobileLink>
+            <MobileLink href='#education' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Education</MobileLink>
+            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank">Github Profile</GitHubButton>
+          </MobileMenu>
+        }
+      </NavbarContainer>
+    </Nav>
+  )
+}
 
-        <ul className='list-none hidden sm:flex flex-row gap-10'>
-          {navLinks.map((nav) => (
-            <li
-              key={nav.id}
-              className={`${
-                active === nav.title ? "text-white" : "text-secondary"
-              } hover:text-white text-[18px] font-medium cursor-pointer`}
-              onClick={() => setActive(nav.title)}
-            >
-              <a href={`#${nav.id}`}>{nav.title}</a>
-            </li>
-          ))}
-        </ul>
-
-        <div className='sm:hidden flex flex-1 justify-end items-center'>
-          <img
-            src={toggle ? close : menu}
-            alt='menu'
-            className='w-[28px] h-[28px] object-contain'
-            onClick={() => setToggle(!toggle)}
-          />
-
-          <div
-            className={`${
-              !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
-          >
-            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
-              {navLinks.map((nav) => (
-                <li
-                  key={nav.id}
-                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                    active === nav.title ? "text-white" : "text-secondary"
-                  }`}
-                  onClick={() => {
-                    setToggle(!toggle);
-                    setActive(nav.title);
-                  }}
-                >
-                  <a href={`#${nav.id}`}>{nav.title}</a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
-};
-
-export default Navbar;
+export default Navbar
