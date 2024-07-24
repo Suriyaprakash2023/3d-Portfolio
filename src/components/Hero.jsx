1260842c6566049e7de900f9215a700b88271a3b
Suriyaprakash2023 <150240754+Suriyaprakash2023@users.noreply.github.com>
2024-07-24 07:36:54 +0530

    skills box set mobile responce bending

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,19 +16,21 @@ import { FaFacebook, FaTwitterSquare } from "react-icons/fa";
 import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { SiLeetcode } from "react-icons/si";
 
+
+
 // import { SiLeetcode } from "react-icons/si";
 // ...
 
 
 const HeroSection = () => {
     return (
-        <div id="about">
-            <HeroContainer>
-                <HeroBg>
+        <div id="about" >
+            <HeroContainer  className='hero-container'>
+                <HeroBg >
                     <HeroBgAnimation />
                 </HeroBg>
                 <HeroInnerContainer >
-                    <HeroLeftContainer id="Left">
+                    <HeroLeftContainer id="Left" className='mt-5'>
                         {/* <Title>Hi, I am <br /> <Span className='pink-font'> {Bio.name} </Span></Title> */}
                         <TextLoop>
                             I am a
@@ -44,7 +46,8 @@ const HeroSection = () => {
                         </TextLoop>
                         <SubTitle>{Bio.description}</SubTitle>
                         {/* <ResumeButton className='gradient-button' href={Bio.resume} target='display'>Check Resume</ResumeButton> */}
-                        <div className="my-3 ">
+               
+                        <div className="flex items-center gap-3 my-3 ">
                             <Link
                             href={Bio.github}
                             target='_blank'
@@ -81,22 +84,38 @@ const HeroSection = () => {
                             <FaTwitterSquare size={30} />
                             </Link>
                         </div>
-                          
-                        <div className="flex items-center gap-3">
+                
+                        <div className="flex items-center gap-3 my-5" id='style-buttons'>
                             <Link href="#contact" className="bg-gradient-to-r to-pink-500 from-violet-600 p-[1px] rounded-full transition-all duration-300 hover:from-pink-500 hover:to-violet-600">
-                            <div className='outline-button'>  
-                                  <button className='btn text-white ' >
-                                    <span>Contact me</span>
-                                    <RiContactsFill size={16} />
-                                </button>
-                            </div>
+                          
+                            <button class="btn" type="button">
+                            <i class="fa-regular fa-user pr-2"></i>
+                                <strong>Contact Me</strong>
+                                <div id="container-stars">
+                                    <div id="stars"></div>
+                                </div>
+
+                                <div id="glow">
+                                    <div class="circle"></div>
+                                    <div class="circle"></div>
+                                </div>
+                            </button>
+
                             </Link>
 
-                            <Link className='gradient-button ms-3' role="button" 
-                            >
-                            <span >Get Resume</span>
-                            <MdDownload size={16} />
+                            <Link className='ms-3' role="button" >
+                            {/* <span >Get Resume</span> */}
+
+
+                             <button class="cssbuttons-io-button">
+                            <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M1 14.5a6.496 6.496 0 0 1 3.064-5.519 8.001 8.001 0 0 1 15.872 0 6.5 6.5 0 0 1-2.936 12L7 21c-3.356-.274-6-3.078-6-6.5zm15.848 4.487a4.5 4.5 0 0 0 2.03-8.309l-.807-.503-.12-.942a6.001 6.001 0 0 0-11.903 0l-.12.942-.805.503a4.5 4.5 0 0 0 2.029 8.309l.173.013h9.35l.173-.013zM13 12h3l-4 5-4-5h3V8h2v4z" fill="currentColor"></path></svg>
+                            <span>Download</span>
+                            </button>
+
+
+                            
                             </Link>
+                            
                         </div>
                     </HeroLeftContainer>
 
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -10,73 +10,83 @@ import "react-vertical-timeline-component/style.min.css";
 
 import { styles } from "../styles";
 // import { experiences } from "../constants";
-import { textVariant } from "../utils/motion";
+// import { textVariant } from "../utils/motion";
 import { MyTheme } from "./themes/default";
+import styled from "styled-components";
+import { skills } from "./data/constants";
 
-
-
-import styled from 'styled-components'
-import { skills } from './data/constants'
+import { fadeIn, textVariant } from "../utils/motion";
+import Tilt from "react-parallax-tilt";
+import image from "./images/HeroImage1.webp";
 
 const Container = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-position: relative;
-z-index: 1;
-align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  z-index: 1;
+  align-items: center;
+`;
 
 const Wrapper = styled.div`
-position: relative;
-display: flex;
-justify-content: space-between;
-align-items: center;
-flex-direction: column;
-width: 100%;
-max-width: 1100px;
-gap: 12px;
-@media (max-width: 960px) {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1100px;
+  gap: 12px;
+  @media (max-width: 960px) {
     flex-direction: column;
-}
-`
+  }
+`;
 
 export const Title = styled.div`
-font-size: 42px;
-text-align: center;
-font-weight: 600;
-margin-top: 20px;
+  font-size: 42px;
+  text-align: center;
+  font-weight: 600;
+  margin-top: 20px;
   color: ${({ theme }) => MyTheme.text_primary};
   @media (max-width: 768px) {
-margin-top: 12px;
-      font-size: 32px;
+    margin-top: 12px;
+    font-size: 32px;
   }
 `;
 
 export const Desc = styled.div`
-    font-size: 18px;
-    text-align: center;
-    max-width: 600px;
-    color: ${({ theme }) => MyTheme.text_secondary};
-    @media (max-width: 768px) {
-        font-size: 16px;
-    }
+  font-size: 18px;
+  text-align: center;
+  max-width: 600px;
+  color: ${({ theme }) => MyTheme.text_secondary};
+  @media (max-width: 768px) {
+    font-size: 16px;
+  }
 `;
 
 const SkillsContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  margin-top: 30px;
-  gap: 30px;
-  justify-content: center;
-`
+
+    display: grid;
+    grid-template-columns: repeat(2, 1fr); /* Two columns grid */
+    gap: 70px; /* Adjust gap as needed */
+    width: 100%;
+  
+    /* Center items in the second row */
+    & > *:nth-child(n+3):nth-child(-n+4) {
+      grid-column: span 2; /* Ensures they span two columns */
+      display: flex;
+      justify-content: center;
+    }
+  }
+  
+
+`;
 
 const Skill = styled.div`
   width: 100%;
   max-width: 500px;
   background: ${({ theme }) => MyTheme.card};
-  border: 0.1px solid #854CE6;
+  border: 0.1px solid #854ce6;
   box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
   border-radius: 16px;
   padding: 18px 36px;
@@ -88,9 +98,7 @@ const Skill = styled.div`
     max-width: 330px;
     padding: 10px 36px;
   }
-
-
-`
+`;
 
 const SkillTitle = styled.h2`
   font-size: 28px;
@@ -98,15 +106,15 @@ const SkillTitle = styled.h2`
   color: ${({ theme }) => MyTheme.text_secondary};
   margin-bottom: 20px;
   text-align: center;
-`
+`;
 
 const SkillList = styled.div`
   display: flex;
-  justify-content: center; 
+  justify-content: center;
   flex-wrap: wrap;
   gap: 12px;
   margin-bottom: 20px;
-`
+`;
 
 const SkillItem = styled.div`
   font-size: 16px;
@@ -127,69 +135,128 @@ const SkillItem = styled.div`
     font-size: 14px;
     padding: 6px 12px;
   }
-`
+`;
 
 const SkillImage = styled.img`
   width: 24px;
   height: 24px;
-`
+`;
+
+
 
 const Tech = () => {
   return (
-    <div>
-    <div className="row">
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {/* {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+    <div className="container">
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+
+        <motion.div variants={textVariant()}>
+          <h2 className={`${styles.sectionHeadText} text-center`}>Skills.</h2>
+        </motion.div>
+
+        <div className='w-full flex justify-center'>
+          <motion.p
+            variants={fadeIn("", "", 0.1, 1)}
+            className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+          >
+            Following projects showcases my skills and experience through
+            real-world examples of my work. Each project is briefly described with
+            links to code repositories and live demos in it. It reflects my
+            ability to solve complex problems, work with different technologies,
+            and manage projects effectively.
+          </motion.p>
         </div>
-      ))} */}
-      
-      <motion.div variants={textVariant()}>
-       
-        <h2 className={`${styles.sectionHeadText} text-center`}>
-          Skills.
-        </h2>
-      </motion.div>
-
-
-    </div>  
-
-
-    </div>
     
-    <div className="row">
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      
-        <Container id="skills">
-       <Wrapper>
-       
-         <Desc>Here are some of my skills on which I have been working on for the past  1 years.
-         </Desc>
-         <SkillsContainer>
-           {skills.map((skill,id) => (
-             <Skill key={skill.id}>
-               <SkillTitle>{skill.title}</SkillTitle>
-               <SkillList>
-                 {skill.skills.map((item,id) => (
-                   <SkillItem className="skill-item" style={{border:'1px solid rgb(104, 201, 246)'}} key={item.id}>
-                     <SkillImage src={item.image}/>
-                     {item.name}
-                   </SkillItem>
-                 ))}
-               </SkillList>
-             </Skill>
-           ))}
+        
+        
+        <Container id="skills" > 
+            <Wrapper>
+              <SkillsContainer>
+                {skills.map((skill, id) => (
+                  <motion.div variants={fadeIn("up", "spring", 1 * 0.5, 0.75)}>
+                    <Tilt
+                      options={{
+                        max: 45,
+                        scale: 1,
+                        speed: 450,
+                      }}
+                      className="bg- p-5 rounded-2xl sm:w-[560px] w-full"
+                    >
+                      
+                      <div className="relative w-full h-[310px]">
+                        <div className="w-full h-full object-cover rounded-2xl">
+                       
+                          <Skill key={skill.id}>
+                          <div className="col-6">
+                            <SkillTitle>{skill.title}</SkillTitle>
+                            <SkillList>
+                              {skill.skills.map((item, id) => (
+                                <SkillItem
+                                  className="skill-item"
+                                  style={{
+                                    border: "1px solid rgb(104, 201, 246)",
+                                  }}
+                                  key={item.id}
+                                >
+                                  <SkillImage src={item.image} />
+                                  {item.name}
+                                </SkillItem>
+                              ))}
+                            </SkillList>
+                            </div>
+                          </Skill>
+                         
+                        </div>
 
-         </SkillsContainer>
-       </Wrapper>
-     </Container>
+
+                      </div>
+                    </Tilt>
+                  </motion.div>
+
+                ))}
+               
+              </SkillsContainer>  
+            </Wrapper>
+          </Container>
+         
+         
 
 
       </div>
     </div>
-    </div>
   );
 };
-
 export default SectionWrapper(Tech, "");
+
+// import './Tech.css';
+// const Tech = () => {
+//   return (
+//     <div className="container">
+//       <div className="row">
+//         <div className="col-6">
+//           row-1 col-1
+//         </div>
+//         <div className="col-6">
+//           row-1 col-2
+//         </div>
+//       </div> 
+
+
+
+
+//       <div className="row">
+//         <div className="col-6">
+//           row-2 col-1
+//         </div>
+//         <div className="col-6">
+//           row-2 col-2
+//         </div>
+//       </div> 
+
+
+
+
+      
+//     </div>
+//   );
+// };
+// export default SectionWrapper(Tech, "");
